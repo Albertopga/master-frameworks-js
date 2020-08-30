3e09f94988be7a37ca0ea07946c04b51844d40b3
Alberto <albertopga@gmail.com>
2020-08-30 17:41:33 +0200

    NodeJs server created

---
+++ Backend/app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// cargar módulos de node para crear el servidor
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// ejecutar express para trabajar con http
+const app = express();
+
+// cargar fichero de rutas
+
+// cargar middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// activar el CORS
+
+// añadir prefijos a  rutas / cargar rutas
+
+// Ruta o metodo de prueba para Api rest
+app.get("/datos-curso", (req, res) => {
+  return res.status(200).send({
+    curso: "Master en framewors",
+    alumno: "Alberto Pérez",
+    url: "https://github.com/Albertopga/master-frameworks-js",
+  });
+});
+// exportar el módulo (fichero actual)
+module.exports = app;
