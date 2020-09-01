12d9343742c213fe79c45453d58e9f7da83ae3cc
Alberto <albertopga@gmail.com>
2020-09-01 18:59:53 +0200

    ngIf and ngFor in component peliculas

--- AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -7,11 +7,19 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
   public titulo: string;
+  public peliculas: Array<any>;
+
+
   /* no se debe meter lógica dentro del constructor, sólo usarlo para asignar 
   valor a las propiedades*/
   constructor() {
     this.titulo = "Componente películas"
-
+    this.peliculas = [
+      { title: "SpiderMan", image: "https://as01.epimg.net/meristation/imagenes/2019/09/27/noticias/1569603388_605931_1569603425_noticia_normal.jpg" },
+      { title: "Vengadores", image: "https://as01.epimg.net/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg" },
+      { title: "IronMan", image: "https://blogdesuperheroes.es/Iron%20Man/BDS_IronMan_Definitivomini.jpg" },
+      { title: "Thor", image: "https://depor.com/resizer/Wbi-oZhBgGCuTLGvU9yWxi6P5w8=/640x0/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/RIWVELPLOZFTBLJBMLAS4TOJZE.jpg" }
+    ]
   }
 
   /*esto es un hook que se ejecuta cuando iniciamos nuestro componente, y lo hace después 
