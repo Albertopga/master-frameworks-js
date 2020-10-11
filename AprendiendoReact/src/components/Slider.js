9ad43b5de87eca68e1b42e72b6e95b10fa004073
Alberto <Albertopga@gmail.com>
2020-10-11 17:50:02 +0200

    .

---
+++ AprendiendoReact/src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Slider() {
+  return (
+    <div id="slider" className="slider-big">
+      <h1>Bienvenido al Curso de React</h1>
+      <a href="#" className="btn-white">
+        Ir al blog
+      </a>
+    </div>
+  );
+}
